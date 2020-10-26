ae877ecb96de3cf29d3dd241b796ac6c6eca511f
johnpospisil <audiofreak7@gmail.com>
2020-10-26 16:02:39 -0700

    Receive only needed data from the API request.

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,12 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
-    return response;
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    const ModifiedData = { confirmed, recovered, deaths, lastUpdate };
+
+    return ModifiedData;
   } catch (error) {}
 };
