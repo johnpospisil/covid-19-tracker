9d9d04c5f4de5c7a089cc582fc69f2a2fb817ea2
johnpospisil <audiofreak7@gmail.com>
2020-10-27 11:38:11 -0700

    Populate the CountryPicker with a list ofcountries

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -14,13 +14,14 @@ const CountryPicker = () => {
     fetchAPI();
   }, [setFetchedCountries]);
 
-console.log(fetchedCountries);
+  // console.log(fetchedCountries);
 
   return (
     <div>
       <FormControl className={styles.formControl}>
         <NativeSelect>
           <option value="global">Global</option>
+          {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
         </NativeSelect>
       </FormControl>
     </div>
